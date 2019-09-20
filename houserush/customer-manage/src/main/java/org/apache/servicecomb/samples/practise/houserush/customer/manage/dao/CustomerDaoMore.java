8fe440f9860c55688bba943caae382de4ec953d5
itcast <1360098430@qq.com>
2019-09-20 18:35:23 +0800

    test

--- houserush/customer-manage/src/main/java/org/apache/servicecomb/samples/practise/houserush/customer/manage/dao/CustomerDaoMore.java
+++ houserush/customer-manage/src/main/java/org/apache/servicecomb/samples/practise/houserush/customer/manage/dao/CustomerDaoMore.java
@@ -2,4 +2,4 @@ package org.apache.servicecomb.samples.practise.houserush.customer.manage.dao;
 
 public interface CustomerDaoMore {
   public void updateCustomerIdUseUseId(int userId,int cid);
-} 
+}
