0eb812268cc4ae045bb37bf0fdefd2957abd1536
itcast <1360098430@qq.com>
2019-09-23 09:15:06 +0800

    删除文件,优化更新代码

--- houserush/realestate/src/main/java/org/apache/servicecomb/samples/practise/houserush/realestate/dao/HouseDao.java
+++ houserush/realestate/src/main/java/org/apache/servicecomb/samples/practise/houserush/realestate/dao/HouseDao.java
@@ -20,13 +20,18 @@ package org.apache.servicecomb.samples.practise.houserush.realestate.dao;
 import org.apache.servicecomb.samples.practise.houserush.realestate.aggregate.House;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.persistence.LockModeType;
 import java.util.List;
 
-public interface HouseDao extends JpaRepository<House, Integer>, HouseDaoMore {
+public interface HouseDao extends JpaRepository<House, Integer> {
   @Lock(LockModeType.PESSIMISTIC_WRITE)
   @Query("SELECT h FROM org.apache.servicecomb.samples.practise.houserush.realestate.aggregate.House h WHERE h.id in (?1)")
   List<House> findAllByIdInForUpdate(List<Integer> ids);
+
+  @Modifying
+  @Query("UPDATE House h set h.state = 'locking' where h.id in (?1)")
+  public int updateLockingStatesForHouses(List<Integer> ids);
 }
